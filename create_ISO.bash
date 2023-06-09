c728f5d6f7fbe11ac4d416c5084dabcd86ecbd28
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-06-09 19:35:46 +0200

    isohybrid

--- create_ISO.bash
+++ create_ISO.bash
@@ -11,6 +11,7 @@ cp preseed.cfg ./iso
 sed -i "s+quiet+quiet priority=high file=/cdrom/preseed.cfg+g" iso/isolinux/txt.cfg
 
 xorriso -as mkisofs -o proxmox_custom.iso \
-        -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
         -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot \
         -boot-load-size 4 -boot-info-table ./iso
+
+isohybrid --uefi ./proxmox_custom.iso
