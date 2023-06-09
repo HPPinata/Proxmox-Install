0fc4e3edad36a169ced6afe548a39fdfcb2cee0d
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-06-09 20:23:02 +0200

    UEFI

--- create_ISO.bash
+++ create_ISO.bash
@@ -10,8 +10,9 @@ cp preseed.cfg ./iso
 
 sed -i "s+quiet+quiet priority=high file=/cdrom/preseed.cfg+g" iso/isolinux/txt.cfg
 
-xorriso -as mkisofs -o proxmox_custom.iso \
-        -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot \
-        -boot-load-size 4 -boot-info-table ./iso
+OUTPUT=proxmox_custom.iso
+VERSION=7.4
+xorrisofs -o $OUTPUT -v -r -J --joliet-long -V "Proxmox $VERSION" -c isolinux/boot.cat -b isolinux/isolinux.bin \
+          -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efiboot.img -no-emul-boot .
 
-isohybrid ./proxmox_custom.iso
+isohybrid --uefi $OUTPUT
